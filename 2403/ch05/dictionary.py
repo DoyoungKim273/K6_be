4e7bd3bda5735aca1b9dec81c70b595703529fac
Doyoung Kim <160606319+DoyoungKim273@users.noreply.github.com>
2024-03-11 10:34:31 +0900

    Update dictionary.py

--- 2403/ch05/dictionary.py
+++ 2403/ch05/dictionary.py
@@ -59,20 +59,31 @@ for color in new_alien['colors']:
     for point in new_alien['points']:
         print(point)
 
+# 딕셔너리 안에 딕셔너리를 중첩 -- 키와 값의 쌍으로 두고 값에 다시 딕셔너리를 중첩함
+# 중첩된 안쪽의 딕셔너리를 부를때는 for 문으로 우선 밖과 안의 딕셔너리를 명명해주어야함 -- 키값쌍을 반환하는 .item() 메서드를 이용
+# for문 안에서 다시 user_info에서 각 값을 선언 -- 출력할 수 있게 됨
+        
 users = {
     'a' : {
         'first' : 'amy',
-        'last' : 'albert'
+        'last' : 'albert',
+        'location' : 'busan'
     },
     'b' : {
         'first' : 'ben',
-        'last' : 'bently'
+        'last' : 'bently',
+        'location' : 'busan'
     },
     'c' : {
         'first' : 'cate',
-        'last' : 'cindy'
-    }
+        'last' : 'cindy',
+        'location' : 'busan'
+    }  
 }
 
-for user_key in users.items():
+for user_key, user_info in users.items():
     print(user_key)
+    full_name = f"{user_info['first']} {user_info['last']}"
+    city = user_info['location']
+    print(full_name.upper())
+    print(city)
